057faafc20f68ee004bc690a9ca76668aa69b22b
anisazahrawidyanti <55775447+anisazahrawidyanti@users.noreply.github.com>
2020-06-12 17:18:17 +0700

    Add files via upload

---
+++ mytoken.py
@@ -0,0 +1,5 @@
+TOKEN='1034301417:AAGpT9CXOMpyKNPvW7mMJUyRUHxM_OtYlrI'
+SAPA="Selamat datang, saya starbhak Bot \U0001F916 \n" \
+     "Aku bisa membantu kamu dengan perintah : \n " \
+     "\U0001F535 /start untuk memulai " \
+     "\n\U0001F4CB /datasiswa untuk melihat data siswa \n"
